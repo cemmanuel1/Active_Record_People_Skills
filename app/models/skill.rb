9d7fd3199a842ab6c4db5d2823c9c60c110d754b
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-07-03 17:07:30 -0700

    complete

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,11 @@
 class Skill < ActiveRecord::Base
-   has_many :users, :through => :user_skills
-    has_many :user_skills
+  has_many :users, through: :skill_users
+  has_many :skill_users
+
+  validates :name, uniqueness: true
+
+  def user_with_proficiency(level)
+    self.skill_users.where(proficiency: level).first.user
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
-  has_many :skills, :through => :user_skills
-  has_many :user_skills
+  has_many :skills, through: :skill_users
+  has_many :skill_users
+
+  def set_proficiency_for(skill, level)
+    user = self.skill_users.where(skill_id: skill.id).first 
+    user.update_attributes(proficiency: level)
+  end
+
+  def proficiency_for(skill)
+   user = self.skill_users.where(skill_id: skill.id).first
+   user.blank? ? 0 : user.proficiency 
+  end 
+  
 end
+
